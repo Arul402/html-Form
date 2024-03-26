cb0e14f79bf4e0d5e0c0f5575f8d0f4b330f9791
Arul Prakasham <88582980+Arul402@users.noreply.github.com>
2024-03-26 12:43:35 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,9 @@
+var em=document.getElementById("Email")
+var ps=document.getElementById("pass")
+function display(){
+    var email=em.value
+    console.log(email)
+    var password=ps.value
+    console.log(password)
+    alert("Your Email: "+email+"\nYour Password: "+password)
+}
